d52fc32a20b64d59e5ae0fea127421dec998641e
Alex Bezek <alex.bezek@cerner.com>
2016-12-19 23:30:09 -0600

    initial changes

---
+++ .chef/cert_knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "as027811"
+client_key               "#{current_dir}/as027811.pem"
+validation_client_name   "main-validator"
+validation_key           "#{current_dir}/main-validator.pem"
+chef_server_url          "https://chef.spherestage.net/organizations/main"
+cookbook_path            ["#{current_dir}/../cookbooks"]
---
+++ install.bash
@@ -0,0 +1,12 @@
+#/bin/zsh
+
+# install git
+# set git config
+# git clone dotfiles repo
+#
+# install atom
+# install berkshelf
+# install bundler
+# install chef
+# install docker
+# 
